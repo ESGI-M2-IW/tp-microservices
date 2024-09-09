ec49e15211148a560cc7ba36d9c157a8231d6c65
Loan <loanbillonnet@gmail.com>
2024-09-09 16:02:31 +0200

    feat: :tada: initialize django project with settings

---
+++ srv-client/config/settings/dev.py
@@ -0,0 +1,12 @@
+from .base import *
+
+DEBUG = True
+
+# Environment information
+ENVIRONMENT_NAME = 'DEV'
+ENVIRONMENT_COLOR = '#38cb65'
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+CORS_ALLOWED_ORIGINS = ['http://localhost']
+
+STATIC_URL = 'static/'
---
+++ srv-client/config/settings/prod.py
@@ -0,0 +1,10 @@
+from .base import *
+
+DEBUG = False
+
+ENVIRONMENT_NAME = 'Production'
+ENVIRONMENT_COLOR = '#b21c17'
+
+ALLOWED_HOSTS = []
+
+CORS_ALLOWED_ORIGINS = []
